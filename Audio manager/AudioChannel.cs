75474fe0dc5a8c232237a6c4578e69dfdd5eba39
DockFrankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2021-02-21 20:38:50 +0100

    Added audio manager

---
+++ Audio manager/AudioChannel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace qASIC.AudioManagment
+{
+    [System.Serializable]
+    public class AudioChannel
+    {
+        public AudioSource source { get; set; }
+        public IEnumerator destroyEnum { get; set; }
+
+        public IEnumerator DestroyOnPlaybackEnd()
+        {
+            if (source == null || source.loop) yield break;
+            float delay = 0.1f;
+            yield return new WaitForSecondsRealtime(source.clip.length - source.time + delay);
+            Object.Destroy(source);
+            source = null;
+            destroyEnum = null;
+        }
+
+        public AudioChannel()
+        {
+            source = null;
+            destroyEnum = null;
+        }
+    }
+}
---
+++ Audio manager/AudioData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace qASIC.AudioManagment
+{
+    [System.Serializable]
+    public class AudioData
+    {
+        public AudioClip clip;
+        public AudioMixerGroup group;
+        public bool loop;
+        public bool replace = true;
+    }
+}
