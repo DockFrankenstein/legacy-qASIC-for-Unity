a5d8c75de21cdeee57f447eacc2496bb4c30bec0
Franciszek Pyrć <franciszekpyrc@gmail.com>
2021-08-31 13:31:57 +0200

    Changed version to v0.1.2

--- Assets/qASIC/Info.cs
+++ Assets/qASIC/Info.cs
@@ -2,8 +2,8 @@
 {
     public static class Info
     {
-        public const string Version = "0.1.1";
-        public const string ConsoleVersion = "1.0.1";
+        public const string Version = "0.1.2";
+        public const string ConsoleVersion = "1.0.2";
         public const string DisplayerVersion = "0.2.1";
         public const string OptionsVersion = "0.1.1";
         public const string InputVersion = "0.0.1";
