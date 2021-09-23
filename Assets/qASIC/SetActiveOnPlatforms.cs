bf54aa4961a1b70c288ac0f2ff8305d256daf87c
Franciszek Pyrć <franciszekpyrc@gmail.com>
2021-09-23 16:00:42 +0200

    Added SetActiveOnPlatforms class

---
+++ Assets/qASIC/SetActiveOnPlatforms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace qASIC
+{
+    public class SetActiveOnPlatforms : MonoBehaviour
+    {
+        [SerializeField] bool state = false;
+        [SerializeField] RuntimePlatform[] platforms = new RuntimePlatform[] { RuntimePlatform.WebGLPlayer };
+
+        private void Awake()
+        {
+            gameObject.SetActive(Array.IndexOf(platforms, Application.platform) != -1 == state);
+        }
+    }
+}
