7f1257083b567af7859d42ff273d714479781800
Franciszek Pyrć <franciszekpyrc@gmail.com>
2022-02-04 11:02:47 +0100

    Updated version

--- Assets/qASIC/Info.cs
+++ Assets/qASIC/Info.cs
@@ -2,12 +2,12 @@
 {
     public static class Info
     {
-        public const string Version = "0.1.4";
-        public const string ConsoleVersion = "1.1.1";
-        public const string DisplayerVersion = "0.2.2";
-        public const string OptionsVersion = "0.1.1";
-        public const string InputVersion = "0.0.1";
-        public const string AudioVersion = "1.1.0";
-        public const string FileVersion = "1.1.0";
+        public const string Version = "0.2.0-dev";
+        public const string ConsoleVersion = "1.1.2";
+        public const string DisplayerVersion = "0.2.3";
+        public const string OptionsVersion = "0.2.0";
+        public const string InputVersion = "1.0.0";
+        public const string AudioVersion = "1.1.1";
+        public const string FileVersion = "1.1.1";
     }
 }
