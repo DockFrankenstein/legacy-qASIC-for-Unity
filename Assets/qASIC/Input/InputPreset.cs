aad241e17f9b0a3453c14f02b722796a675015da
Dock Frankenstein <franciszekpyrc@gmail.com>
2022-03-15 12:06:25 +0100

    Removed legacy input preset from create menu

--- Assets/qASIC/Input/InputPreset.cs
+++ Assets/qASIC/Input/InputPreset.cs
@@ -1,11 +1,8 @@
 ﻿//Removed in v0.2.0
+//This scriptable object is obselete and has been replaced with the Input Map
 using UnityEngine;
 
 namespace qASIC.InputManagement
 {
-    [CreateAssetMenu(fileName = "NewInputPreset", menuName = "qASIC/Input/Input Preset")]
-    public class InputPreset : ScriptableObject
-    {
-        //This scriptable object is obselete and has been replaced with the Input Map
-    }
+    public class InputPreset : ScriptableObject { }
 }
