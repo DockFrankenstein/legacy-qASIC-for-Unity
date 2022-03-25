8c74fbd7651bc02390f1ad92bb82d41646826497
Dock Frankenstein <franciszekpyrc@gmail.com>
2022-03-25 20:19:19 +0100

    Updated version to v0.2.0

--- Assets/qASIC/Info.cs
+++ Assets/qASIC/Info.cs
@@ -2,7 +2,7 @@
 {
     public static class Info
     {
-        public const string Version = "0.2.0-dev";
+        public const string Version = "0.2.0";
         public const string ConsoleVersion = "1.1.2";
         public const string DisplayerVersion = "0.2.3";
         public const string OptionsVersion = "0.2.0";
