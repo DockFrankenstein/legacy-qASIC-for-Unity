f8bb35089d3a570245e47d59cc5ef460c34d920b
DockFrankenstein <franciszekpyrc@gmail.com>
2022-07-03 22:24:46 +0200

    Added menu execute command script

---
+++ Assets/qASIC/MenuExecuteCommand.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using qASIC.Console;
+
+namespace qASIC
+{
+    public class MenuExecuteCommand : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            Button button = GetComponent<Button>();
+            if (button == null) return;
+
+            UnityEditor.Events.UnityEventTools.AddStringPersistentListener(button.onClick, Execute, "");
+        }
+#endif
+
+        public void Execute(string cmd)
+        {
+            GameConsoleController.RunCommand(cmd);
+        }
+    }
+}
