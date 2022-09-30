ffd9786495b0e46b374db902fe19e1d4fa556e4e
DockFrankenstein <franciszekpyrc@gmail.com>
2022-09-30 07:26:48 +0200

    Updated versions

--- Assets/qASIC/Info.cs
+++ Assets/qASIC/Info.cs
@@ -4,11 +4,11 @@ namespace qASIC.Internal
 {
     public static class Info
     {
-        public const string Version = "0.2.2";
+        public const string Version = "0.3.0-dev";
         public const string ConsoleVersion = "1.1.4";
         public const string DisplayerVersion = "0.2.4";
         public const string OptionsVersion = "0.3.0";
-        public const string InputVersion = "1.0.3";
+        public const string InputVersion = "2.0.0-dev";
         public const string AudioVersion = "1.2.0";
         public const string FileVersion = "1.1.1";
 
